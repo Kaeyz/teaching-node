c1f5f1bfef87d1619c52799e79568f23eaba6c72
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-05-28 13:16:12 +0100

    new feature add: add function can take 3 parameters

--- app.js
+++ app.js
@@ -9,4 +9,4 @@ console.log(typeof Number(process.argv[2]))
 console.log(typeof Number(process.argv[3]))
 
 
-add(+process.argv[2], +process.argv[3])
+add(+process.argv[2], +process.argv[3], +process.argv[4])
--- day1.js
+++ day1.js
@@ -3,11 +3,11 @@ var name = 'Kolade'
 
 console.log(`Hello, ${name}!! ${greeting}`)
 
-exports.add = function(a, b) {
-  if((typeof a === 'number') && (typeof b === 'number')) {
+exports.add = function(a, b, c) {
+  if((typeof a === 'number') && (typeof b === 'number') && (typeof c === 'number')) {
     // console.log(assert.equal(a, b))
     
-    console.log(`Sum of ${a} + ${b} = ${a+b}`)
+    console.log(`Sum of ${a} + ${b} + ${c} = ${a+b+c}`)
   } else {
     console.log('we are not numbers')
   }
