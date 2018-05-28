9eaec680408c970c3c5726a32e0af66112457810
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-05-28 16:46:09 +0100

    comment our console.log

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var { add } = require('./day1.js')
 // import * as alias from 'module';
 // import {  } from "module";
 
-console.log(typeof Number(process.argv[2]))
-console.log(typeof Number(process.argv[3]))
+// console.log(typeof Number(process.argv[2]))
+// console.log(typeof Number(process.argv[3]))
 
 
 add(+process.argv[2], +process.argv[3], +process.argv[4])
--- day1.js
+++ day1.js
@@ -4,7 +4,9 @@ var name = 'Kolade'
 console.log(`Hello, ${name}!! ${greeting}`)
 
 exports.add = function(a, b, c) {
-  if((typeof a === 'number') && (typeof b === 'number') && (typeof c === 'number')) {
+  if((typeof a === 'number') 
+  && (typeof b === 'number') 
+  && (typeof c === 'number')) {
     // console.log(assert.equal(a, b))
     
     console.log(`Sum of ${a} + ${b} + ${c} = ${a+b+c}`)
